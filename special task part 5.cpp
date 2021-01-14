cf160bed125b44b77b608e046d0379271f57e9fc
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-01-14 17:14:22 +0500

    Add files via upload

---
+++ special task part 5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int num;
+ cout<<"Entre the Number :";
+ cin>>num;
+ if(num%3)
+ {
+ cout<<"The number is even.";
+ }else if(num%2)
+ {
+ cout<<" The number is odd.";
+ }
+}
